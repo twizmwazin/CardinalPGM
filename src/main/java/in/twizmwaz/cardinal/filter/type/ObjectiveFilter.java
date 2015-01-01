748959483d936f0b19ec69bd0832c33998573c25
twizmwazin <twizmwazin@gmail.com>
2015-01-01 12:42:37 -0500

    Add support for Objective filters

--- src/main/java/in/twizmwaz/cardinal/filter/type/ObjectiveFilter.java
+++ src/main/java/in/twizmwaz/cardinal/filter/type/ObjectiveFilter.java
@@ -2,6 +2,7 @@ package in.twizmwaz.cardinal.filter.type;
 
 import in.twizmwaz.cardinal.filter.Filter;
 import in.twizmwaz.cardinal.filter.FilterState;
+import in.twizmwaz.cardinal.module.GameObjective;
 import org.bukkit.Bukkit;
 
 import java.util.logging.Level;
@@ -10,10 +11,16 @@ import static in.twizmwaz.cardinal.filter.FilterState.*;
 
 public class ObjectiveFilter extends Filter {
 
+    private final GameObjective objective;
+
+    public ObjectiveFilter(final GameObjective objective) {
+        this.objective = objective;
+    }
+
     @Override
     public FilterState getState(final Object o) {
-        Bukkit.getLogger().log(Level.INFO, "Objective Filters are not yet supported in CardinalPGM.");
-        return ABSTAIN;
+        if (objective.isComplete()) return ALLOW;
+        else return DENY;
     }
 
 }
