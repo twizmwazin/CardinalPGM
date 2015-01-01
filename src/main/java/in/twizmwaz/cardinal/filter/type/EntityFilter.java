8da3a7210f30d5469c5a0097aa37c9377b178582
twizmwazin <twizmwazin@gmail.com>
2015-01-01 12:26:51 -0500

    Corrects Entity related filters

--- src/main/java/in/twizmwaz/cardinal/filter/type/EntityFilter.java
+++ src/main/java/in/twizmwaz/cardinal/filter/type/EntityFilter.java
@@ -2,21 +2,21 @@ package in.twizmwaz.cardinal.filter.type;
 
 import in.twizmwaz.cardinal.filter.Filter;
 import in.twizmwaz.cardinal.filter.FilterState;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 
 import static in.twizmwaz.cardinal.filter.FilterState.*;
 
 public class EntityFilter extends Filter {
 
-    private final Entity entity;
+    private final EntityType entity;
 
-    public EntityFilter(final Entity mob) {
+    public EntityFilter(final EntityType mob) {
         this.entity = mob;
     }
 
     @Override
     public FilterState getState(final Object o) {
-        if (o instanceof Entity) {
+        if (o instanceof EntityType) {
             if (o.equals(entity)) return ALLOW;
             else return DENY;
         }
