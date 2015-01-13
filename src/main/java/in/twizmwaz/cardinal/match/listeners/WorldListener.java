68ef6d806bac960c129ccd644260cca317fa4196
iEli2tyree011 <connorjayr@gmail.com>
2015-01-12 21:35:38 -0500

    Add various events to manage the world

---
+++ src/main/java/in/twizmwaz/cardinal/match/listeners/WorldListener.java
@@ -0,0 +1,37 @@
+package in.twizmwaz.cardinal.match.listeners;
+
+import in.twizmwaz.cardinal.match.Match;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.LightningStrikeEvent;
+import org.bukkit.event.weather.ThunderChangeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.event.world.StructureGrowEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldListener implements Listener {
+
+    private final JavaPlugin plugin;
+    private final Match match;
+
+    public WorldListener(JavaPlugin plugin, Match match) {
+        this.plugin = plugin;
+        this.match = match;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onWeatherChange(WeatherChangeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onLightningStrike(LightningStrikeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onThunderChange(ThunderChangeEvent event) {
+        event.setCancelled(true);
+    }
+}
