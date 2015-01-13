55f5ffb21d6cdbe8f1cf6cee185947762ca225c1
iEli2tyree011 <connorjayr@gmail.com>
2015-01-13 18:29:24 -0500

    Fix for the tool repair volume! :)

--- src/main/java/in/twizmwaz/cardinal/module/modules/toolRepair/ToolRepair.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/toolRepair/ToolRepair.java
@@ -52,7 +52,7 @@ public class ToolRepair implements Module {
                     if (itemsMatch) {
                         event.setCancelled(true);
                         event.getItem().remove();
-                        event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ITEM_PICKUP, 10, 1);
+                        event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ITEM_PICKUP, 0.1F, 1);
                         toRepair.setDurability((short) (toRepair.getDurability() - (item1.getType().getMaxDurability() - item1.getDurability()) < 0 ? 0 : toRepair.getDurability() - (item1.getType().getMaxDurability() - item1.getDurability())));
                     }
                 }
