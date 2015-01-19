b03805e756553d99f8bfcd45e9098825641fbd00
iEli2tyree011 <connorjayr@gmail.com>
2015-01-19 12:33:32 -0500

    Add a match timer.

---
+++ src/main/java/in/twizmwaz/cardinal/module/modules/matchTimer/MatchTimerBuilder.java
@@ -0,0 +1,16 @@
+package in.twizmwaz.cardinal.module.modules.matchTimer;
+
+import in.twizmwaz.cardinal.match.Match;
+import in.twizmwaz.cardinal.module.ModuleBuilder;
+import in.twizmwaz.cardinal.module.ModuleCollection;
+
+public class MatchTimerBuilder implements ModuleBuilder {
+
+    @Override
+    public ModuleCollection load(Match match) {
+        ModuleCollection results = new ModuleCollection();
+        results.add(new MatchTimer(match));
+        return results;
+    }
+
+}
