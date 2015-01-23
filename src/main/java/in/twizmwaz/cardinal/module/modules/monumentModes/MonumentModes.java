423678a9cb897b53987449868ae7acecdb037134
iEli2tyree011 <connorjayr@gmail.com>
2015-01-22 21:22:30 -0500

    Add the actual monument mode classes

---
+++ src/main/java/in/twizmwaz/cardinal/module/modules/monumentModes/MonumentModes.java
@@ -0,0 +1,65 @@
+package in.twizmwaz.cardinal.module.modules.monumentModes;
+
+import com.sk89q.minecraft.util.commands.ChatColor;
+import in.twizmwaz.cardinal.GameHandler;
+import in.twizmwaz.cardinal.module.GameObjective;
+import in.twizmwaz.cardinal.module.TaskedModule;
+import in.twizmwaz.cardinal.module.modules.cores.CoreObjective;
+import in.twizmwaz.cardinal.module.modules.destroyable.DestroyableObjective;
+import in.twizmwaz.cardinal.module.modules.matchTimer.MatchTimer;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+
+public class MonumentModes implements TaskedModule {
+
+    private final int after;
+    private final Material material;
+    private final int damageValue;
+    private final String name;
+
+    private boolean ran;
+
+    public MonumentModes(final int after, final Material material, final int damageValue, final String name) {
+        this.after = after;
+        this.material = material;
+        this.damageValue = damageValue;
+        this.name = name;
+
+        this.ran = false;
+    }
+
+    @Override
+    public void unload() {
+        HandlerList.unregisterAll(this);
+    }
+
+    @Override
+    public void run() {
+        if (GameHandler.getGameHandler().getMatch().isRunning()) {
+            if (!this.ran && MatchTimer.getTimeInSeconds() >= this.after) {
+                for (CoreObjective core : GameHandler.getGameHandler().getMatch().getModules().getModules(CoreObjective.class)) {
+                    if (core.changesModes()) {
+                        for (Block block : core.getBlocks()) {
+                            block.setType(this.material);
+                            core.setMaterial(this.material, this.damageValue);
+                        }
+                    }
+                }
+                for (DestroyableObjective destroyable : GameHandler.getGameHandler().getMatch().getModules().getModules(DestroyableObjective.class)) {
+                    if (destroyable.changesModes()) {
+                        for (Block block : destroyable.getBlocks()) {
+                            block.setType(this.material);
+                            destroyable.setMaterial(this.material, this.damageValue);
+                        }
+                    }
+                }
+                Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "> > > > " + ChatColor.RED + name + ChatColor.DARK_AQUA + " < < < <");
+                this.ran = true;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/in/twizmwaz/cardinal/module/modules/monumentModes/MonumentModesBuilder.java
@@ -0,0 +1,50 @@
+package in.twizmwaz.cardinal.module.modules.monumentModes;
+
+import com.sk89q.minecraft.util.commands.ChatColor;
+import in.twizmwaz.cardinal.GameHandler;
+import in.twizmwaz.cardinal.match.Match;
+import in.twizmwaz.cardinal.module.BuilderData;
+import in.twizmwaz.cardinal.module.ModuleBuilder;
+import in.twizmwaz.cardinal.module.ModuleCollection;
+import in.twizmwaz.cardinal.module.ModuleLoadTime;
+import in.twizmwaz.cardinal.module.modules.cores.CoreObjective;
+import in.twizmwaz.cardinal.util.StringUtils;
+import org.bukkit.Material;
+import org.jdom2.Element;
+
+@BuilderData(load = ModuleLoadTime.LATEST)
+public class MonumentModesBuilder implements ModuleBuilder {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public ModuleCollection load(Match match) {
+        ModuleCollection results = new ModuleCollection();
+        for (Element modes : match.getDocument().getRootElement().getChildren("modes")) {
+            for (Element mode : modes.getChildren("mode")) {
+                int after = StringUtils.timeStringToSeconds(mode.getAttributeValue("after"));
+                Material material;
+                int damageValue = 0;
+                if (mode.getAttributeValue("material").contains(":")) {
+                    material = Material.matchMaterial(mode.getAttributeValue("material").split(":")[0]);
+                    damageValue = Integer.parseInt(mode.getAttributeValue("material").split(":")[1]);
+                } else {
+                    material = Material.matchMaterial(mode.getAttributeValue("material"));
+                }
+                String name = "MODE CHANGE";
+                if (mode.getAttributeValue("name") != null) {
+                    name = ChatColor.translateAlternateColorCodes('`', mode.getAttributeValue("name"));
+                }
+                results.add(new MonumentModes(after, material, damageValue, name));
+            }
+        }
+        boolean core = match.getModules().getModules(CoreObjective.class).size() > 0;
+        if (results.size() == 0 && core) {
+            for (CoreObjective coreObjective : match.getModules().getModules(CoreObjective.class)) {
+                coreObjective.setChangesModes(true);
+            }
+            results.add(new MonumentModes(900, Material.GOLD_BLOCK, 0, "GOLD CORE MODE"));
+            results.add(new MonumentModes(1200, Material.GLASS, 0, "GLASS CORE MODE"));
+        }
+        return results;
+    }
+}
