5a74fa37b81914af44999d8d6f7d19c50127b2f4
Elliott Whyamn <elliottwhyman@hotmail.co.uk>
2015-02-05 18:05:51 +0000

    fix Errors

--- src/main/java/in/twizmwaz/cardinal/module/modules/blockdrops/Blockdrops.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/blockdrops/Blockdrops.java
@@ -6,6 +6,7 @@ import in.twizmwaz.cardinal.module.modules.filter.FilterModule;
 import in.twizmwaz.cardinal.module.modules.filter.FilterState;
 import in.twizmwaz.cardinal.module.modules.regions.RegionModule;
 import in.twizmwaz.cardinal.module.modules.regions.type.BlockRegion;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.ExperienceOrb;
@@ -18,10 +19,14 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.LazyMetadataValue;
 import org.bukkit.util.Vector;
 
+import java.sql.BatchUpdateException;
 import java.util.Random;
 import java.util.Set;
+import java.util.logging.Level;
 
 public class Blockdrops implements Module {
 
@@ -89,26 +94,49 @@ public class Blockdrops implements Module {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityExplode(EntityExplodeEvent event) {
         if (fallchance != 0.0) {
-            if (region.contains(new BlockRegion(null, event.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5))))) {
-                double fallchance = event.blockList().size() * this.fallchance;
-                for (double i = fallchance; i > 0; i--) {
-                    Block block = event.blockList().get((int) i - 1);
-                    Vector vector = Vector.getRandom();
-                    FallingBlock fallingBlock = GameHandler.getGameHandler().getMatchWorld().spawnFallingBlock(event.getLocation(), block.getType(), block.getData());
-                    fallingBlock.setVelocity(vector);
-                    fallingBlock.setDropItem(false);
-                }
+            if (region != null) {
+                if (region.contains(new BlockRegion(null, event.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5))))) {
+                    double fallchance = event.blockList().size() * this.fallchance;
+                    for (double i = fallchance; i > 0; i--) {
+                        Block block = event.blockList().get((int) i);
+                        if (!filter.evaluate(block).equals(FilterState.DENY)) {
+                            Vector vector = Vector.getRandom();
+                            FallingBlock fallingBlock = GameHandler.getGameHandler().getMatchWorld().spawnFallingBlock(event.getLocation(), block.getType(), block.getData());
+                            fallingBlock.setMetadata(block.getType().name(), new FixedMetadataValue(GameHandler.getGameHandler().getPlugin(), "material"));
+                            fallingBlock.setVelocity(vector);
+                            fallingBlock.setDropItem(false);
+                        }
+                    }
 
+                }
+            } else {
+                double flyingBlocks = event.blockList().size() * this.fallchance;
+                for (double i = flyingBlocks - 1; i > 0; i--) {
+                    Block block = event.blockList().get((int) i);
+                    if (!filter.evaluate(block).equals(FilterState.DENY)) {
+                        Vector vector = Vector.getRandom();
+                        FallingBlock fallingBlock = GameHandler.getGameHandler().getMatchWorld().spawnFallingBlock(event.getLocation(), block.getType(), block.getData());
+                        fallingBlock.setMetadata("falling-block", new FixedMetadataValue(GameHandler.getGameHandler().getPlugin(), i));
+                        fallingBlock.setVelocity(vector);
+                        fallingBlock.setDropItem(false);
+
+                    }
+                }
             }
         }
     }
 
-    @EventHandler
-    public void onEntiyChangeBlock(EntityChangeBlockEvent event) {
-        if (event.getEntity() instanceof FallingBlock) {
-            if (new Random().nextGaussian() > landchance)
-                event.setCancelled(true);
+        @EventHandler
+        public void onEntiyChangeBlock (EntityChangeBlockEvent event) {
+            if (event.getEntity() instanceof FallingBlock) {
+                if (event.getEntity().hasMetadata("falling-block")) {
+                    if (event.getEntity().getMetadata("falling-block").get(0).asInt() <= new Random().nextInt() * this.fallchance) {
+                        event.setCancelled(true);
+                    }
+                }
+
+            }
         }
-    }
+
 }
 
