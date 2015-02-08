5102bb3c0ab17e08771a39e7beb7525d99f3cfa1
iEli2tyree011 <connorjayr@gmail.com>
2015-02-08 01:15:13 -0500

    Work on includes, left debug messages inside (for later)

--- src/main/java/in/twizmwaz/cardinal/util/DomUtils.java
+++ src/main/java/in/twizmwaz/cardinal/util/DomUtils.java
@@ -7,33 +7,35 @@ import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.util.logging.Level;
 
 public class DomUtils {
 
     public static Document parse(File file) throws JDOMException, IOException {
         SAXBuilder saxBuilder = new SAXBuilder();
         Document original = saxBuilder.build(file);
-        Path repo = file.getParentFile().getParentFile().toPath();
+        Bukkit.getLogger().info("1");
         for (Element include : original.getRootElement().getChildren("include")) {
+            Bukkit.getLogger().info("2");
+            File path = file.getParentFile();
+            Bukkit.getLogger().info("3");
+            String source = include.getAttributeValue("src");
+            Bukkit.getLogger().info("4");
+            while (source.contains("../")) {
+                source = source.replace("../", "");
+                path = path.getParentFile();
+            }
+            Bukkit.getLogger().info("5");
+            File including = new File(path, source);
+            Bukkit.getLogger().info("6");
             try {
-                Path included;
-                try {
-                    included = repo.resolve(include.getAttributeValue("src")).normalize();
-                    for (Element element : parse(included.toFile()).getRootElement().getChildren()) {
-                        original.getRootElement().addContent(element.detach());
-                    }
-                } catch (FileNotFoundException e) {
-                    included = file.getParentFile().toPath().resolve(include.getAttributeValue("src")).normalize();
-                    for (Element element : parse(included.toFile()).getRootElement().getChildren()) {
-                        original.getRootElement().addContent(element.detach());
-                        Bukkit.getLogger().info(element.getName());
-                    }
+                for (Element element : parse(including).getRootElement().getChildren()) {
+                    Bukkit.getLogger().info(element.getName() + "");
+                    original.getRootElement().addContent(element.detach());
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (JDOMException | IOException e) {
+                Bukkit.getLogger().log(Level.WARNING, "File '" + including.getName() + "' was not included correctly!");
             }
         }
         return original;
