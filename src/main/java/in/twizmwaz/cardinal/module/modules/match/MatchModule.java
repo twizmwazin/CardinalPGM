275163c8f980ddcb8955dc48573956fc72fc31dd
GuiMan <guimanpg@gmail.com>
2015-02-11 20:05:36 +0100

    Change the order of cycling messages.

--- src/main/java/in/twizmwaz/cardinal/module/modules/match/MatchModule.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/match/MatchModule.java
@@ -55,9 +55,9 @@ public class MatchModule implements Module {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onCycleComplete(CycleCompleteEvent event) {
+    Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "Cycled to " + ChatColor.AQUA + event.getMatch().getMapInfo().getName());
         for (Player player : Bukkit.getOnlinePlayers()) {
             TeamUtils.getTeamById("observers").add(player, true);
         }
-        Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "Cycled to " + ChatColor.AQUA + event.getMatch().getMapInfo().getName());
     }
 }
