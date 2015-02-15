c80695557f021d597160491efa6650b42e3733e0
Elliott Whyamn <elliottwhyman@hotmail.co.uk>
2015-02-15 17:59:50 +0000

    Add book support for kits

---
+++ src/main/java/in/twizmwaz/cardinal/module/modules/kit/KitBook.java
@@ -0,0 +1,45 @@
+package in.twizmwaz.cardinal.module.modules.kit;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+class KitBook {
+
+    private String title;
+    private String author;
+    private List<String> pages;
+    private int slot;
+
+    protected KitBook(String title, String author, List<String> pages, int slot) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.slot = slot;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPage(int index) {
+        return pages.get(index);
+    }
+
+    public List<String> getPages(){
+        return pages;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public boolean hasSlot() {
+        return slot != -1;
+    }
+
+}
