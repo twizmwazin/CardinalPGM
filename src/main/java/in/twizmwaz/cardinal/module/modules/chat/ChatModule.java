22947078ed402f3c1c998d144057d56b5e955651
Elliott Whyamn <elliottwhyman@hotmail.co.uk>
2015-02-16 20:34:23 +0000

    Log default chat

--- src/main/java/in/twizmwaz/cardinal/module/modules/chat/ChatModule.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/chat/ChatModule.java
@@ -5,6 +5,8 @@ import in.twizmwaz.cardinal.module.Module;
 import in.twizmwaz.cardinal.module.modules.chatChannels.TeamChannel;
 import in.twizmwaz.cardinal.util.TeamUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -21,8 +23,11 @@ public class ChatModule implements Module {
         event.setCancelled(true);
         //get default channel
         for (TeamChannel channel : GameHandler.getGameHandler().getMatch().getModules().getModules(TeamChannel.class)) {
-            if (channel.getTeam().equals(TeamUtils.getTeamByPlayer(event.getPlayer()))) 
+            if (channel.getTeam().equals(TeamUtils.getTeamByPlayer(event.getPlayer()))) {
                 channel.sendMessage(channel.getTeam().getColor() + "[Team] " + event.getPlayer().getDisplayName() + ChatColor.RESET + ": " + event.getMessage());
+                ConsoleCommandSender console = GameHandler.getGameHandler().getPlugin().getServer().getConsoleSender();
+                console.sendMessage(channel.getTeam().getColor() + "[" + channel.getTeam().getName() + " Team] " + event.getPlayer().getDisplayName() + ChatColor.RESET + ": " + event.getMessage());
+            }
         }
     }
 }
