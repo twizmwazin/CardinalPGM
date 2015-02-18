84397752e46ffc9e15753f01971f9acfe7674f0b
iEli2tyree011 <connorjayr@gmail.com>
2015-02-18 14:22:52 -0500

    Stop the match timer when the match ends

--- src/main/java/in/twizmwaz/cardinal/module/modules/matchTimer/MatchTimer.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/matchTimer/MatchTimer.java
@@ -1,18 +1,22 @@
 package in.twizmwaz.cardinal.module.modules.matchTimer;
 
 import in.twizmwaz.cardinal.GameHandler;
+import in.twizmwaz.cardinal.event.MatchEndEvent;
 import in.twizmwaz.cardinal.event.MatchStartEvent;
 import in.twizmwaz.cardinal.match.Match;
 import in.twizmwaz.cardinal.match.MatchState;
 import in.twizmwaz.cardinal.module.Module;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 
 public class MatchTimer implements Module {
 
     private long startTime;
+    private double endTime;
 
     protected MatchTimer() {
+        this.endTime = 0;
     }
 
     @Override
@@ -25,6 +29,11 @@ public class MatchTimer implements Module {
         this.startTime = System.currentTimeMillis();
     }
 
+    @EventHandler
+    public void onMatchEnd(MatchEndEvent event) {
+        this.endTime = ((double) System.currentTimeMillis() - (GameHandler.getGameHandler().getMatch().getModules().getModule(MatchTimer.class)).getTime()) / 1000.0;
+    }
+
     /**
      * @return The current time stored in the module.
      */
@@ -32,12 +41,17 @@ public class MatchTimer implements Module {
         return startTime;
     }
 
+    public double getEndTime() {
+        return endTime;
+    }
+
     public static double getTimeInSeconds() {
         Match match = GameHandler.getGameHandler().getMatch();
-        if (match.isRunning() || match.getState().equals(MatchState.ENDED) || match.getState().equals(MatchState.CYCLING)) {
-            for (MatchTimer timer : GameHandler.getGameHandler().getMatch().getModules().getModules(MatchTimer.class)) {
-                return ((double) System.currentTimeMillis() - timer.getTime()) / 1000.0;
-            }
+        if (match.isRunning()) {
+            return ((double) System.currentTimeMillis() - (GameHandler.getGameHandler().getMatch().getModules().getModule(MatchTimer.class)).getTime()) / 1000.0;
+        }
+        if (match.getState().equals(MatchState.ENDED) || match.getState().equals(MatchState.CYCLING)) {
+            return GameHandler.getGameHandler().getMatch().getModules().getModule(MatchTimer.class).getEndTime();
         }
         return 0;
     }
