e78acfeb8e6d53ec4a6bbba5094c5fc5315a9ed5
Kevin Phoenix <twizmwazin@gmail.com>
2015-02-22 12:02:47 -0500

    Add config option to disable MOTD, closes #243

--- src/main/java/in/twizmwaz/cardinal/module/modules/motd/MOTDBuilder.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/motd/MOTDBuilder.java
@@ -1,5 +1,6 @@
 package in.twizmwaz.cardinal.module.modules.motd;
 
+import in.twizmwaz.cardinal.Cardinal;
 import in.twizmwaz.cardinal.match.Match;
 import in.twizmwaz.cardinal.module.ModuleBuilder;
 import in.twizmwaz.cardinal.module.ModuleCollection;
@@ -8,8 +9,8 @@ public class MOTDBuilder implements ModuleBuilder {
 
     @Override
     public ModuleCollection load(Match match) {
-        ModuleCollection results = new ModuleCollection();
-        results.add(new MOTD(match));
+        ModuleCollection<MOTD> results = new ModuleCollection<MOTD>();
+        if (Cardinal.getInstance().getConfig().getBoolean("custom-motd")) results.add(new MOTD(match));
         return results;
     }
 
