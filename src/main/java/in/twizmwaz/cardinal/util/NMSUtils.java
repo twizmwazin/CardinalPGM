8c8634aef00e83cbc8d77b976f8d7bded37842ab
Kevin Phoenix <twizmwazin@gmail.com>
2015-02-26 17:51:45 -0500

    Add basic NMS/OBC util

---
+++ src/main/java/in/twizmwaz/cardinal/util/NMSUtils.java
@@ -0,0 +1,29 @@
+package in.twizmwaz.cardinal.util;
+
+import org.bukkit.Bukkit;
+
+public class NMSUtils {
+    
+    public static Class<?> getNMS(String name) {
+        try {
+            return Class.forName("net.minecraft.server." + name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static Class<?> getCraft(String name) {
+        try {
+            return Class.forName("org.bukkit.craftbukkit." + getVersion() + (name.startsWith(".") ? name : "." + name));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().replace(".", "," + "" + "").split(",")[3];
+    }
+    
+}
