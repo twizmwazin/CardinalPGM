3ccd33dbd04afa20199e26c8022d96efc4fdb9ff
Alfalik <Alfalik@users.noreply.github.com>
2015-03-21 21:57:32 -0400

    Fix wool touch message & remove permissions in proximity command

--- src/main/java/in/twizmwaz/cardinal/command/ProximityCommand.java
+++ src/main/java/in/twizmwaz/cardinal/command/ProximityCommand.java
@@ -20,17 +20,17 @@ public class ProximityCommand {
 
     @Command(aliases = {"proximity"}, desc = "Shows the proximity of the objectives in the match.")
     public static void ready(final CommandContext cmd, CommandSender sender) throws CommandException {
-        if (!(sender instanceof Player) || TeamUtils.getTeamByPlayer((Player) sender) == null || TeamUtils.getTeamByPlayer((Player) sender).isObserver() || !GameHandler.getGameHandler().getMatch().isRunning() || sender.hasPermission("cardinal.proximity")) {
+        if (!(sender instanceof Player) || TeamUtils.getTeamByPlayer((Player) sender) == null || TeamUtils.getTeamByPlayer((Player) sender).isObserver() || !GameHandler.getGameHandler().getMatch().isRunning()) {
             for (TeamModule team : TeamUtils.getTeams()) {
                 if (!team.isObserver()) {
                     sender.sendMessage(team.getCompleteName());
                     for (GameObjective objective : TeamUtils.getShownObjectives(team)) {
                         if (objective.isComplete()) {
-                            sender.sendMessage(" " + WordUtils.capitalizeFully(objective.getName().replaceAll("_", " ")) + " " + ChatColor.GREEN + "COMPLETE");
+                            sender.sendMessage("  " + WordUtils.capitalizeFully(objective.getName().replaceAll("_", " ")) + " " + ChatColor.GREEN + "COMPLETE");
                         } else if (objective.isTouched()) {
                             if (objective instanceof WoolObjective) {
                                 double proximity = ((WoolObjective) objective).getProximity();
-                                sender.sendMessage("  " + WordUtils.capitalizeFully(objective.getName().replaceAll("_", " ")) + "  " + ChatColor.RED + "UNTOUCHED" + ChatColor.GRAY + "  closest safety: " + ChatColor.AQUA + (proximity == Double.POSITIVE_INFINITY ? "n/a" : (Math.round(proximity * 100.0) / 100.0)));
+                                sender.sendMessage("  " + WordUtils.capitalizeFully(objective.getName().replaceAll("_", " ")) + "  " + ChatColor.YELLOW + "TOUCHED" + ChatColor.GRAY + "  closest safety: " + ChatColor.AQUA + (proximity == Double.POSITIVE_INFINITY ? "n/a" : (Math.round(proximity * 100.0) / 100.0)));
                             } else {
                                 sender.sendMessage("  " + WordUtils.capitalizeFully(objective.getName().replaceAll("_", " ")) + "  " + ChatColor.YELLOW + "TOUCHED");
                             }
@@ -41,7 +41,7 @@ public class ProximityCommand {
                     }
                 }
             }
-        } else throw new CommandPermissionsException();
+        } else throw new CommandException("The /proximity command is only avaible to observers");
     }
 
 }
