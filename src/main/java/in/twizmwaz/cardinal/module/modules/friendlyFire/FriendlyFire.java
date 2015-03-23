ffd8063258764fa64d994c59a3204a4d24ab7aa7
Elliott Whyamn <elliottwhyman@hotmail.co.uk>
2015-03-23 18:43:09 +0000

    Prevent players leaving team and getting team arrow kills, fixes #445

--- src/main/java/in/twizmwaz/cardinal/module/modules/friendlyFire/FriendlyFire.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/friendlyFire/FriendlyFire.java
@@ -6,7 +6,13 @@ import in.twizmwaz.cardinal.module.Module;
 import in.twizmwaz.cardinal.module.modules.scoreboard.ScoreboardModule;
 import in.twizmwaz.cardinal.module.modules.team.TeamModule;
 import in.twizmwaz.cardinal.util.TeamUtils;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class FriendlyFire implements Module {
 
@@ -20,6 +26,26 @@ public class FriendlyFire implements Module {
             for (TeamModule team : TeamUtils.getTeams()) {
                 for (ScoreboardModule scoreboard : GameHandler.getGameHandler().getMatch().getModules().getModules(ScoreboardModule.class)) {
                     scoreboard.getScoreboard().getTeam(team.getId()).setAllowFriendlyFire(false);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBowShootEvent(EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player) {
+            event.getEntity().setMetadata("team", new FixedMetadataValue(GameHandler.getGameHandler().getPlugin(), TeamUtils.getTeamByPlayer(((Player) event.getEntity()).getPlayer()).getName()));
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Arrow) {
+            if (((Arrow) event.getDamager()).getShooter() instanceof Player) {
+                if (event.getDamager().hasMetadata("team")) {
+                    Player shooter = (Player) ((Arrow) event.getDamager()).getShooter();
+                    if (TeamUtils.getTeamByPlayer(shooter).getName().equals(event.getDamager().getMetadata("team").get(0).toString()));
+                    event.setCancelled(true);
                 }
             }
         }
