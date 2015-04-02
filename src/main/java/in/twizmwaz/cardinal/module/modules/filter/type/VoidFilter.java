37277e6ecf518f0826c3f70bcda187a295c9ce85
iEli2tyree011 <connorjayr@gmail.com>
2015-04-02 16:54:52 -0400

    Make sure that the block being evaluated in a void filter is not the same as the block it's compared to, fixes #282

--- src/main/java/in/twizmwaz/cardinal/module/modules/filter/type/VoidFilter.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/filter/type/VoidFilter.java
@@ -4,6 +4,7 @@ import in.twizmwaz.cardinal.GameHandler;
 import in.twizmwaz.cardinal.module.modules.filter.FilterModule;
 import in.twizmwaz.cardinal.module.modules.filter.FilterState;
 import in.twizmwaz.cardinal.module.modules.filter.parsers.GenericFilterParser;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -20,9 +21,9 @@ public class VoidFilter extends FilterModule {
     public FilterState evaluate(final Object... objects) {
         for (Object object : objects) {
             if (object instanceof Block) {
-                Block check = new Location(GameHandler.getGameHandler().getMatchWorld(),
-                        ((Block) object).getX(), 0, ((Block) object).getZ()).getBlock();
-                return check.getType() == Material.AIR && !check.equals(object) ? (getParent() == null ? ALLOW : (getParent().evaluate(objects).equals(DENY) ? DENY : ALLOW)) : DENY;
+                Block check = new Location(GameHandler.getGameHandler().getMatchWorld(), ((Block) object).getX(), 0, ((Block) object).getZ()).getBlock();
+                if (object.equals(check)) return getParent() == null ? ALLOW : (getParent().evaluate(objects).equals(DENY) ? DENY : ALLOW);
+                return check.getType() == Material.AIR ? (getParent() == null ? ALLOW : (getParent().evaluate(objects).equals(DENY) ? DENY : ALLOW)) : DENY;
             }
         }
         return (getParent() == null ? ABSTAIN : getParent().evaluate(objects));
