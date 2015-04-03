d9719c7660cbed587b26489dd04f67ef19f11746
iEli2tyree011 <connorjayr@gmail.com>
2015-04-02 22:05:48 -0400

    Use metadata to track "rage arrows", fixes #517

--- src/main/java/in/twizmwaz/cardinal/module/modules/rage/Rage.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/rage/Rage.java
@@ -1,14 +1,18 @@
 package in.twizmwaz.cardinal.module.modules.rage;
 
+import in.twizmwaz.cardinal.GameHandler;
 import in.twizmwaz.cardinal.module.Module;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class Rage implements Module{
 
@@ -27,16 +31,16 @@ public class Rage implements Module{
                 }
             }
         } else if (event.getDamager() instanceof Arrow) {
-            if (((Arrow) event.getDamager()).getShooter() instanceof Player) {
-                Player shooter = (Player) ((Arrow) event.getDamager()).getShooter();
-                for (ItemStack item : shooter.getInventory()) {
-                    if (item != null) {
-                        if (item.getType().equals(Material.BOW) && item.containsEnchantment(Enchantment.ARROW_DAMAGE) && item.getEnchantmentLevel(Enchantment.ARROW_DAMAGE) > 1) {
-                            event.setDamage(1000);
-                        }
-                    }
-                }
+            if (event.getDamager().hasMetadata("rage")) {
+                event.setDamage(1000);
             }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityBowShoot(EntityShootBowEvent event) {
+        if (event.getBow() != null && event.getBow().containsEnchantment(Enchantment.ARROW_DAMAGE) && event.getBow().getEnchantmentLevel(Enchantment.ARROW_DAMAGE) > 1) {
+            event.getProjectile().setMetadata("rage", new FixedMetadataValue(GameHandler.getGameHandler().getPlugin(), true));
         }
     }
 }
