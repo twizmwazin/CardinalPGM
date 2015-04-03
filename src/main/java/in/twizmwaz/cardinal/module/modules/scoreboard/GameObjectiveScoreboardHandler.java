2b7b9d8b7711472a7c77c51893e901cec8bf6cb5
iEli2tyree011 <connorjayr@gmail.com>
2015-04-03 17:08:45 -0400

    Fix spacing for prefix in wools

--- src/main/java/in/twizmwaz/cardinal/module/modules/scoreboard/GameObjectiveScoreboardHandler.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/scoreboard/GameObjectiveScoreboardHandler.java
@@ -37,7 +37,7 @@ public class GameObjectiveScoreboardHandler {
             if (wool.isComplete()) {
                 prefix = MiscUtils.convertDyeColorToChatColor(wool.getColor()) + " \u2B1B ";
             } else if (wool.isTouched() && (this.team == team || team.isObserver())) {
-                prefix = MiscUtils.convertDyeColorToChatColor(wool.getColor()) + " \u2592" + (wool.showProximity() ? " " + ChatColor.RESET + NumUtils.convertToSubscript(wool.getProximity() == Double.POSITIVE_INFINITY || wool.getProximity() == Double.NEGATIVE_INFINITY ? wool.getProximity() : Math.round(wool.getProximity() * 10.0) / 10.0) + " " : "");
+                prefix = MiscUtils.convertDyeColorToChatColor(wool.getColor()) + " \u2592 " + (wool.showProximity() ? " " + ChatColor.RESET + NumUtils.convertToSubscript(wool.getProximity() == Double.POSITIVE_INFINITY || wool.getProximity() == Double.NEGATIVE_INFINITY ? wool.getProximity() : Math.round(wool.getProximity() * 10.0) / 10.0) + " " : "");
             } else if (this.team == team || team.isObserver()) {
                 prefix = MiscUtils.convertDyeColorToChatColor(wool.getColor()) + " \u2B1C " + (wool.showProximity() ? ChatColor.RESET + NumUtils.convertToSubscript(wool.getProximity() == Double.POSITIVE_INFINITY || wool.getProximity() == Double.NEGATIVE_INFINITY ? wool.getProximity() : Math.round(wool.getProximity() * 10.0) / 10.0) + " " : "");
             } else {
