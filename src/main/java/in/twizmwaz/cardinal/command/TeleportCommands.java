901e35ef57a7cf3047d74b4cc082368c3fdc6633
iEli2tyree011 <connorjayr@gmail.com>
2015-04-06 07:13:34 -0400

    Add another usage to the teleport command, fixes #554

--- src/main/java/in/twizmwaz/cardinal/command/TeleportCommands.java
+++ src/main/java/in/twizmwaz/cardinal/command/TeleportCommands.java
@@ -12,7 +12,7 @@ import org.bukkit.entity.Player;
 
 public class TeleportCommands {
 
-    @Command(aliases = {"tp", "teleport"}, desc = "Teleport players.", usage = "<player> [to, x] [y] [z]", min = 1, max = 4)
+    @Command(aliases = {"tp", "teleport"}, desc = "Teleport players.", usage = "<player> [player], [player] [[~]x] [[~]y] [[~]z]", min = 1, max = 4)
     public static void teleport(final CommandContext cmd, CommandSender sender) throws CommandException {
         if (sender instanceof Player) {
             if (cmd.argsLength() == 1) {
@@ -40,11 +40,30 @@ public class TeleportCommands {
                 } else {
                     throw new CommandPermissionsException();
                 }
+            } else if (cmd.argsLength() == 3) {
+                if (sender.hasPermission("cardinal.teleport")) {
+                    double x = cmd.getString(0).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(0).replaceAll("~", ""));
+                    double y = cmd.getString(1).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(1).replaceAll("~", ""));
+                    double z = cmd.getString(2).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(2).replaceAll("~", ""));
+                    if (cmd.getString(0).contains("~")) x += ((Player) sender).getLocation().getX();
+                    if (cmd.getString(1).contains("~")) y += ((Player) sender).getLocation().getY();
+                    if (cmd.getString(2).contains("~")) z += ((Player) sender).getLocation().getZ();
+                    ((Player) sender).teleport(new Location(((Player) sender).getWorld(), x, y, z, ((Player) sender).getLocation().getYaw(), ((Player) sender).getLocation().getPitch()));
+                    sender.sendMessage(ChatColor.YELLOW + "Teleported.");
+                } else {
+                    throw new CommandPermissionsException();
+                }
             } else if (cmd.argsLength() == 4) {
                 if (sender.hasPermission("cardinal.teleport")) {
                     try {
                         Player teleporting = Bukkit.getPlayer(cmd.getString(0));
-                        teleporting.teleport(new Location(teleporting.getWorld(), (cmd.getString(1).contains("~") ? NumUtils.parseDouble(cmd.getString(1).replaceAll("~", "")) + teleporting.getLocation().getX() : cmd.getDouble(1)), (cmd.getString(2).contains("~") ? NumUtils.parseDouble(cmd.getString(2).replaceAll("~", "")) + teleporting.getLocation().getY() : cmd.getDouble(2)), (cmd.getString(3).contains("~") ? NumUtils.parseDouble(cmd.getString(3).replaceAll("~", "")) + teleporting.getLocation().getZ() : cmd.getDouble(3)), teleporting.getLocation().getYaw(), teleporting.getLocation().getPitch()));
+                        double x = cmd.getString(1).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(1).replaceAll("~", ""));
+                        double y = cmd.getString(2).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(2).replaceAll("~", ""));
+                        double z = cmd.getString(3).equals("~") ? 0 : NumUtils.parseDouble(cmd.getString(3).replaceAll("~", ""));
+                        if (cmd.getString(1).contains("~")) x += teleporting.getLocation().getX();
+                        if (cmd.getString(2).contains("~")) y += teleporting.getLocation().getY();
+                        if (cmd.getString(3).contains("~")) z += teleporting.getLocation().getZ();
+                        teleporting.teleport(new Location(teleporting.getWorld(), x, y, z, teleporting.getLocation().getYaw(), teleporting.getLocation().getPitch()));
                         sender.sendMessage(ChatColor.YELLOW + "Teleported.");
                     } catch (NullPointerException e) {
                         throw new CommandException(new LocalizedChatMessage(ChatConstant.ERROR_NO_PLAYER_MATCH).getMessage(((Player) sender).getLocale()));
