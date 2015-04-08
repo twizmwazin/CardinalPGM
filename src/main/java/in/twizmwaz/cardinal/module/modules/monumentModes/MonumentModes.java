19cae1900fb6c765b598d8f760af79ba7a35961a
dentmaged <jeffcockw@gmail.com>
2015-04-08 22:43:27 +0100

    Add monument mode updates - required later for boss bar

--- src/main/java/in/twizmwaz/cardinal/module/modules/monumentModes/MonumentModes.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/monumentModes/MonumentModes.java
@@ -1,12 +1,16 @@
 package in.twizmwaz.cardinal.module.modules.monumentModes;
 
 import in.twizmwaz.cardinal.GameHandler;
+import in.twizmwaz.cardinal.chat.ChatConstant;
+import in.twizmwaz.cardinal.chat.ChatMessage;
+import in.twizmwaz.cardinal.chat.LocalizedChatMessage;
 import in.twizmwaz.cardinal.chat.UnlocalizedChatMessage;
 import in.twizmwaz.cardinal.module.TaskedModule;
 import in.twizmwaz.cardinal.module.modules.cores.CoreObjective;
 import in.twizmwaz.cardinal.module.modules.destroyable.DestroyableObjective;
 import in.twizmwaz.cardinal.module.modules.matchTimer.MatchTimer;
 import in.twizmwaz.cardinal.util.ChatUtils;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -14,18 +18,19 @@ import org.bukkit.event.HandlerList;
 
 public class MonumentModes implements TaskedModule {
 
-    private int after;
+    private int after, showBefore;
     private final Material material;
     private final int damageValue;
     private final String name;
 
     private boolean ran;
 
-    public MonumentModes(int after, final Material material, final int damageValue, final String name) {
+    public MonumentModes(int after, final Material material, final int damageValue, final String name, int showBefore) {
         this.after = after;
         this.material = material;
         this.damageValue = damageValue;
         this.name = name;
+        this.showBefore = showBefore;
 
         this.ran = false;
     }
@@ -38,6 +43,13 @@ public class MonumentModes implements TaskedModule {
     @Override
     public void run() {
         if (GameHandler.getGameHandler().getMatch().isRunning()) {
+            if (!this.ran && MatchTimer.getTimeInSeconds() - this.after <= showBefore) {
+                int time = (int) (MatchTimer.getTimeInSeconds() - this.after);
+                if (time % 5 == 0 || time <= 5) {
+                    ChatUtils.getGlobalChannel().sendLocalizedMessage(new UnlocalizedChatMessage(ChatColor.WHITE + "{0}", new LocalizedChatMessage(ChatConstant.UI_CORE_MODE, name)));
+                }
+            }
+
             if (!this.ran && MatchTimer.getTimeInSeconds() >= this.after) {
                 for (CoreObjective core : GameHandler.getGameHandler().getMatch().getModules().getModules(CoreObjective.class)) {
                     if (core.changesModes()) {
