cd05e45a6fc96b230e4c36838b6e70e8fc228567
Kevin Phoenix <twizmwazin@gmail.com>
2015-05-22 18:53:42 -0400

    Disallow muted players to use /me command

--- src/main/java/in/twizmwaz/cardinal/module/modules/chat/ChatModule.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/chat/ChatModule.java
@@ -1,10 +1,16 @@
 package in.twizmwaz.cardinal.module.modules.chat;
 
+import in.twizmwaz.cardinal.GameHandler;
+import in.twizmwaz.cardinal.chat.ChatConstant;
+import in.twizmwaz.cardinal.chat.LocalizedChatMessage;
 import in.twizmwaz.cardinal.module.Module;
+import in.twizmwaz.cardinal.module.modules.permissions.PermissionModule;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class ChatModule implements Module {
 
@@ -20,6 +26,15 @@ public class ChatModule implements Module {
             Bukkit.dispatchCommand(event.getPlayer(), String.valueOf(event.getPlayer().getMetadata("default-channel").get(0).asString().charAt(0)) + " " + event.getMessage());
         } else {
             Bukkit.dispatchCommand(event.getPlayer(), "t " + event.getMessage());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+        PermissionModule module = GameHandler.getGameHandler().getMatch().getModules().getModule(PermissionModule.class);
+        if (module.isMuted(event.getPlayer()) && event.getMessage().startsWith("/me ")) {
+            event.getPlayer().sendMessage(ChatColor.RED + new LocalizedChatMessage(ChatConstant.ERROR_NO_PERMISSION).getMessage(event.getPlayer().getLocale()));
+            event.setCancelled(true);
         }
     }
 }
