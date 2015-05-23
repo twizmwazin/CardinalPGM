d44a94e6f75c9db1df28e5a499161455114a93be
iEli2tyree011 <connorjayr@gmail.com>
2015-05-23 13:05:10 -0400

    Fix replacement for blockdrops not working, fixes #652

--- src/main/java/in/twizmwaz/cardinal/module/modules/blockdrops/Blockdrops.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/blockdrops/Blockdrops.java
@@ -21,6 +21,9 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Blockdrops implements Module {
@@ -46,30 +49,33 @@ public class Blockdrops implements Module {
         this.wrongTool = wrongTool;
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onBlockBreak(BlockBreakEvent event) {
         if (!event.isCancelled()) {
             Player player = event.getPlayer();
             Block block = event.getBlock();
-            if ((filter == null || filter.evaluate(player, block).equals(FilterState.ALLOW)) && !player.getGameMode().equals(GameMode.CREATIVE)) {
-                if (region == null || region.contains(new BlockRegion(null, block.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5))))) {
-                    if (!this.wrongTool) {
-                        if (block.getDrops() != null && block.getDrops().size() > 0) {
+            if ((filter == null || filter.evaluate(player, block, event).equals(FilterState.ALLOW))) {
+                if (region == null || region.contains(block.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5)))) {
+                    if (!player.getGameMode().equals(GameMode.CREATIVE)) {
+                        if (!this.wrongTool) {
+                            if (block.getDrops() != null && block.getDrops().size() > 0) {
+                                for (ItemStack drop : this.drops) {
+                                    GameHandler.getGameHandler().getMatchWorld().dropItemNaturally(block.getLocation().clone().add(.5, .5, .5), drop);
+                                }
+                                if (this.experience != 0) {
+                                    ExperienceOrb xp = GameHandler.getGameHandler().getMatchWorld().spawn(block.getLocation(), ExperienceOrb.class);
+                                    xp.setExperience(this.experience);
+                                }
+                            }
+                        } else {
                             for (ItemStack drop : this.drops) {
                                 GameHandler.getGameHandler().getMatchWorld().dropItemNaturally(block.getLocation().clone().add(.5, .5, .5), drop);
                             }
-                            if (this.experience != 0) {
-                                ExperienceOrb xp = GameHandler.getGameHandler().getMatchWorld().spawn(block.getLocation(), ExperienceOrb.class);
-                                xp.setExperience(this.experience);
-                            }
+                            ExperienceOrb xp = GameHandler.getGameHandler().getMatchWorld().spawn(block.getLocation(), ExperienceOrb.class);
+                            xp.setExperience(this.experience);
                         }
-                    } else {
-                        for (ItemStack drop : this.drops) {
-                            GameHandler.getGameHandler().getMatchWorld().dropItemNaturally(block.getLocation().clone().add(.5, .5, .5), drop);
-                        }
-                        ExperienceOrb xp = GameHandler.getGameHandler().getMatchWorld().spawn(block.getLocation(), ExperienceOrb.class);
-                        xp.setExperience(this.experience);
                     }
+                    event.setCancelled(true);
                     block.setType(replace);
                 }
             }
@@ -81,9 +87,10 @@ public class Blockdrops implements Module {
         if (!event.isCancelled()) {
             Player player = TntTracker.getWhoPlaced(event.getEntity()) != null && Bukkit.getOfflinePlayer(TntTracker.getWhoPlaced(event.getEntity())).isOnline() ? Bukkit.getPlayer(TntTracker.getWhoPlaced(event.getEntity())) : null;
             if (player != null) {
+                List<Block> toRemove = new ArrayList<>();
                 for (Block block : event.blockList()) {
-                    if (filter == null || filter.evaluate(player, block).equals(FilterState.ALLOW)) {
-                        if (region == null || region.contains(new BlockRegion(null, block.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5))))) {
+                    if (filter == null || filter.evaluate(player, block, event).equals(FilterState.ALLOW)) {
+                        if (region == null || region.contains(block.getLocation().toVector().add(new Vector(0.5, 0.5, 0.5)))) {
                             for (ItemStack drop : this.drops) {
                                 GameHandler.getGameHandler().getMatchWorld().dropItemNaturally(block.getLocation(), drop);
                             }
@@ -91,10 +98,12 @@ public class Blockdrops implements Module {
                                 ExperienceOrb xp = GameHandler.getGameHandler().getMatchWorld().spawn(block.getLocation(), ExperienceOrb.class);
                                 xp.setExperience(this.experience);
                             }
+                            toRemove.add(block);
                             block.setType(replace);
                         }
                     }
                 }
+                event.blockList().removeAll(toRemove);
             }
         }
     }
