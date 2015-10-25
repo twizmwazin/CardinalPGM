91745cf56bbf0cf20b28101ca6a3d8a9bf64df5e
Alfalik <pablofpizarro@gmail.com>
2015-10-25 01:04:32 -0300

    Use regions also deny block break and block place events

--- src/main/java/in/twizmwaz/cardinal/module/modules/appliedRegion/type/UseRegion.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/appliedRegion/type/UseRegion.java
@@ -8,6 +8,8 @@ import in.twizmwaz.cardinal.module.modules.regions.type.BlockRegion;
 import in.twizmwaz.cardinal.util.ChatUtil;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class UseRegion extends AppliedRegion {
@@ -20,6 +22,22 @@ public class UseRegion extends AppliedRegion {
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.getClickedBlock() == null || (event.getItem() != null && event.getItem().getType().isBlock())) return;
         if (region.contains(new BlockRegion(null, event.getClickedBlock().getLocation().toVector())) && (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) && filter.evaluate(event.getPlayer(), event.getClickedBlock(), event).equals(FilterState.DENY)) {
+            event.setCancelled(true);
+            ChatUtil.sendWarningMessage(event.getPlayer(), message);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerBreakBlock(BlockBreakEvent event) {
+        if (region.contains(new BlockRegion(null, event.getBlock().getLocation().toVector())) && filter.evaluate(event.getPlayer(), event.getBlock(), event).equals(FilterState.DENY)) {
+            event.setCancelled(true);
+            ChatUtil.sendWarningMessage(event.getPlayer(), message);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerPlaceBlock(BlockPlaceEvent event) {
+        if (region.contains(new BlockRegion(null, event.getBlockAgainst().getLocation().toVector())) && filter.evaluate(event.getPlayer(), event.getBlockAgainst(), event).equals(FilterState.DENY)) {
             event.setCancelled(true);
             ChatUtil.sendWarningMessage(event.getPlayer(), message);
         }
