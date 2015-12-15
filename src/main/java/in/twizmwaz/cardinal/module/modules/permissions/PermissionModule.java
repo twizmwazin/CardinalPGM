ab03b0cdf7c4037a7eb3f255b796033535f94f04
Kevin <twizmwazin@gmail.com>
2015-12-14 21:51:27 -0500

    Update developers list

--- src/main/java/in/twizmwaz/cardinal/module/modules/permissions/PermissionModule.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/permissions/PermissionModule.java
@@ -21,14 +21,25 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.Plugin;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 
 public class PermissionModule implements Module {
 
     private final Plugin plugin;
     private final Map<Player, PermissionAttachment> attachmentMap;
 
-    private List<UUID> developers = Arrays.asList(UUID.fromString("670223bb-7560-48c8-8f01-2f463549b917") /* twiz_mwazin */, UUID.fromString("33a703d0-3237-4337-9ddd-3dbf33b3d8a6") /* iEli2tyree011 */, UUID.fromString("208c84af-790a-41da-bf7e-eb184f17bdf8") /* Elly */, UUID.fromString("260004f0-996b-4539-ba21-df4ee6336b63") /* Elliott_ */);
+    private List<UUID> developers = Arrays.asList(
+            UUID.fromString("670223bb-7560-48c8-8f01-2f463549b917") /* twiz_mwazin */,
+            UUID.fromString("33a703d0-3237-4337-9ddd-3dbf33b3d8a6") /* iEli2tyree011 */,
+            UUID.fromString("208c84af-790a-41da-bf7e-eb184f17bdf8") /* Elly */,
+            UUID.fromString("260004f0-996b-4539-ba21-df4ee6336b63") /* Elliott_ */,
+            UUID.fromString("e82603df-417d-4a95-bd1f-1720648be0b4") /* Pablete1234 */);
     private List<OfflinePlayer> muted = new ArrayList<>();
 
     public PermissionModule(Plugin plugin) {
