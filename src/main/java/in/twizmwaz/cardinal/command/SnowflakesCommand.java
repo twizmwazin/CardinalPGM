d2bcffff376025f28efbd01c395e3bfc240e69f7
Andrew <andrew.pheasey@gmail.com>
2016-02-18 16:41:26 +0000

    Fix empty count displayed on snowflakes command.
    - Display a String of 0 if the return value is null/"" (player has no
    snowflakes).
    - Reduced the method to only check the db once.

--- src/main/java/in/twizmwaz/cardinal/command/SnowflakesCommand.java
+++ src/main/java/in/twizmwaz/cardinal/command/SnowflakesCommand.java
@@ -22,10 +22,12 @@ public class SnowflakesCommand {
             Bukkit.dispatchCommand(sender, "snowflakes " + sender.getName());
         } else {
             OfflinePlayer player = Bukkit.getOfflinePlayer(cmd.getString(0));
+            String snowflakes = Cardinal.getCardinalDatabase().get(player, "snowflakes");
+            if (snowflakes.equals("")) snowflakes = "0";
             if (sender.equals(player)) {
-                sender.sendMessage(new UnlocalizedChatMessage(ChatColor.DARK_PURPLE + "{0} " + ChatColor.GOLD + "{1} {2}", ChatConstant.MISC_YOU_HAVE.asMessage(), new UnlocalizedChatMessage("{0}", Cardinal.getCardinalDatabase().get(player, "snowflakes")), Cardinal.getCardinalDatabase().get(player, "snowflakes").equals("1") ? ChatConstant.SNOWFLAKES_SNOWFLAKE.asMessage() : ChatConstant.SNOWFLAKES_SNOWFLAKES.asMessage()).getMessage(ChatUtil.getLocale(sender)));
+                sender.sendMessage(new UnlocalizedChatMessage(ChatColor.DARK_PURPLE + "{0} " + ChatColor.GOLD + "{1} {2}", ChatConstant.MISC_YOU_HAVE.asMessage(), new UnlocalizedChatMessage("{0}", snowflakes), snowflakes.equals("1") ? ChatConstant.SNOWFLAKES_SNOWFLAKE.asMessage() : ChatConstant.SNOWFLAKES_SNOWFLAKES.asMessage()).getMessage(ChatUtil.getLocale(sender)));
             } else {
-                sender.sendMessage(new UnlocalizedChatMessage(ChatColor.DARK_PURPLE + "{0} " + ChatColor.GOLD + "{1} {2}", new LocalizedChatMessage(ChatConstant.MISC_HAS, Players.getName(player) + ChatColor.DARK_PURPLE), new UnlocalizedChatMessage("{0}", Cardinal.getCardinalDatabase().get(player, "snowflakes")), Cardinal.getCardinalDatabase().get(player, "snowflakes").equals("1") ? ChatConstant.SNOWFLAKES_SNOWFLAKE.asMessage() : ChatConstant.SNOWFLAKES_SNOWFLAKES.asMessage()).getMessage(ChatUtil.getLocale(sender)));
+                sender.sendMessage(new UnlocalizedChatMessage(ChatColor.DARK_PURPLE + "{0} " + ChatColor.GOLD + "{1} {2}", new LocalizedChatMessage(ChatConstant.MISC_HAS, Players.getName(player) + ChatColor.DARK_PURPLE), new UnlocalizedChatMessage("{0}", snowflakes), snowflakes.equals("1") ? ChatConstant.SNOWFLAKES_SNOWFLAKE.asMessage() : ChatConstant.SNOWFLAKES_SNOWFLAKES.asMessage()).getMessage(ChatUtil.getLocale(sender)));
             }
         }
     }
