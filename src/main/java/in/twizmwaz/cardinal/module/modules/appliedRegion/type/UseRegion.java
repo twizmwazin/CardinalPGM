29fc5341b77436624da6f534694ca045c19aed72
Pablete1234 <pabloherrerapalacio@gmail.com>
2016-02-23 20:45:46 +0100

    Use regions should only deny using inventories

--- src/main/java/in/twizmwaz/cardinal/module/modules/appliedRegion/type/UseRegion.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/appliedRegion/type/UseRegion.java
@@ -6,10 +6,9 @@ import in.twizmwaz.cardinal.module.modules.filter.FilterState;
 import in.twizmwaz.cardinal.module.modules.regions.RegionModule;
 import in.twizmwaz.cardinal.module.modules.regions.type.BlockRegion;
 import in.twizmwaz.cardinal.util.ChatUtil;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class UseRegion extends AppliedRegion {
@@ -20,26 +19,12 @@ public class UseRegion extends AppliedRegion {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getClickedBlock() == null || (event.getItem() != null && event.getItem().getType().isBlock())) return;
-        if (region.contains(new BlockRegion(null, event.getClickedBlock().getLocation().toVector())) && (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) && filter.evaluate(event.getPlayer(), event.getClickedBlock(), event).equals(FilterState.DENY)) {
-            event.setCancelled(true);
+        if (event.getClickedBlock() == null || !event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
+        if (region.contains(new BlockRegion(null, event.getClickedBlock().getLocation().toVector())) && filter.evaluate(event.getPlayer(), event.getClickedBlock(), event).equals(FilterState.DENY)) {
+            event.setUseItemInHand(Event.Result.ALLOW);
+            event.setUseInteractedBlock(Event.Result.DENY);
             ChatUtil.sendWarningMessage(event.getPlayer(), message);
         }
     }
 
-    @EventHandler
-    public void onPlayerBreakBlock(BlockBreakEvent event) {
-        if (region.contains(new BlockRegion(null, event.getBlock().getLocation().toVector())) && filter.evaluate(event.getPlayer(), event.getBlock(), event).equals(FilterState.DENY)) {
-            event.setCancelled(true);
-            ChatUtil.sendWarningMessage(event.getPlayer(), message);
-        }
-    }
-
-    @EventHandler
-    public void onPlayerPlaceBlock(BlockPlaceEvent event) {
-        if (region.contains(new BlockRegion(null, event.getBlockAgainst().getLocation().toVector())) && filter.evaluate(event.getPlayer(), event.getBlockAgainst(), event).equals(FilterState.DENY)) {
-            event.setCancelled(true);
-            ChatUtil.sendWarningMessage(event.getPlayer(), message);
-        }
-    }
 }
