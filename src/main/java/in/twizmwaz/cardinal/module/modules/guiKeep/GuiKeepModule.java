3b666b687a8ff18046840871e7f680121a94d5c8
Pablete1234 <pabloherrerapalacio@gmail.com>
2016-04-23 04:16:09 +0200

    Prevent Title respawn Armor Stand showing health in GUI

--- src/main/java/in/twizmwaz/cardinal/module/modules/guiKeep/GuiKeepModule.java
+++ src/main/java/in/twizmwaz/cardinal/module/modules/guiKeep/GuiKeepModule.java
@@ -1,10 +1,8 @@
 package in.twizmwaz.cardinal.module.modules.guiKeep;
 
-import com.google.common.base.Optional;
 import in.twizmwaz.cardinal.GameHandler;
 import in.twizmwaz.cardinal.module.Module;
-import in.twizmwaz.cardinal.module.modules.team.TeamModule;
-import in.twizmwaz.cardinal.util.Teams;
+import in.twizmwaz.cardinal.module.modules.observers.ObserverModule;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -20,8 +18,7 @@ public class GuiKeepModule implements Module {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        Optional<TeamModule> team = Teams.getTeamByPlayer(event.getPlayer());
-        if (GameHandler.getGameHandler().getMatch().isRunning() && (!team.isPresent() || !team.orNull().isObserver())) {
+        if (GameHandler.getGameHandler().getMatch().isRunning() && !ObserverModule.testObserverOrDead(event.getPlayer())) {
             if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
                 if (event.getClickedBlock() != null) {
                     if (event.getClickedBlock().getType().equals(Material.WORKBENCH)) {
