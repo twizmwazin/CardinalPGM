652003f76a595da0b225d38a1f617f966d84a9da
Pablete1234 <pabloherrerapalacio@gmail.com>
2016-11-22 23:24:50 +0100

    Update protocol list

--- src/main/java/in/twizmwaz/cardinal/util/Protocols.java
+++ src/main/java/in/twizmwaz/cardinal/util/Protocols.java
@@ -1,7 +1,7 @@
 package in.twizmwaz.cardinal.util;
 
 public enum Protocols {
-
+    VER_1_11("1.11.0", 315),
     VER_1_10("1.10.0-2", 210),
     VER_1_10_PRE2("1.10-pre2", 205),
     VER_1_10_PRE1("1.10-pre1", 204),
@@ -11,16 +11,14 @@ public enum Protocols {
     VER_1_9_3("1.9.3-4", 110),
     VER_1_9_2("1.9.2", 109),
     VER_1_9_1("1.9.1 / 1.RV-Pre1", 108),
-    VER_1_9_0("1.9.0", 107),
-    VER_1_9_PRE4("1.9-pre4", 106),
-    VER_1_9_PRE3("1.9-pre3", 105),
-    VER_1_9_PRE2("1.9-pre2", 104),
-    VER_1_9_PRE1("1.9-pre1", 103);
+    VER_1_9_0("1.9.0", 107);
+
+    public static final int LATEST = 315;
 
     private final String name;
     private final int proto;
 
-    private Protocols(String name, int proto) {
+    Protocols(String name, int proto) {
         this.name = name;
         this.proto = proto;
     }
@@ -31,7 +29,7 @@ public enum Protocols {
                 return proto.name;
             }
         }
-        return null;
+        return ver > LATEST ? "Future" : "Unknown";
     }
 
 
